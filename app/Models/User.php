f4bdefa61c47825ba4e9d306418bcb34455bfc76
salmaemam <salma-emam@moltaqa.net>
2025-12-15 14:31:22 +0200

    feat(client) image

--- app/Models/User.php
+++ app/Models/User.php
@@ -127,7 +127,7 @@ class User extends Authenticatable implements HasMedia
      protected function image(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->getFirstMediaUrl('client-image') ?: '',
+        get: fn() => $this->getFirstMediaUrl('client-image') ?: asset('storage/9/download.jpg'),
         );
     }
 
