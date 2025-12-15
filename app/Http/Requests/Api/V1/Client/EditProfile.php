1c4d99151f8087440d5df06cf0843113b6af4b6e
salmaemam <salma-emam@moltaqa.net>
2025-12-15 15:57:05 +0200

    fix(client-auth) profile edit

--- app/Http/Requests/Api/V1/Client/EditProfile.php
+++ app/Http/Requests/Api/V1/Client/EditProfile.php
@@ -11,7 +11,7 @@ class EditProfile extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -63,7 +63,7 @@ Route::prefix("auth")->group(function () {
 Route::middleware(["auth:api"])->group(function () {
 
     #User data 
-    Route::post("update_profile",[AuthController::class,"updateProfile"]);
+    Route::post("update_profile", [AuthController::class,"updateProfile"]);
     # Chat
     include __DIR__ . '/chat.php';
 });
